e2486d94c4a6ad58cc536614fef87913c5982e8f
MahdiSohaily <mahdi.sohaily4030@gmail.com>
2022-10-10 22:01:39 +0430

    Update check game function

--- src/index.js
+++ src/index.js
@@ -1,22 +1,47 @@
 /* eslint-disable linebreak-style */
 import './CSS/styles.css';
-
+const axios = require('axios').default;
 const form = document.querySelector('.form');
 const nameInput = document.querySelector('.name');
 const scoreInput = document.querySelector('.score');
+const gameID = (() => localStorage.getItem('game'))();
 
-const existGame = () => (localStorage.getItem('game') ? JSON.parse(localStorage.getItem('game')) : false);
-const setGame = () => {
-    
+const setGame = async (name = 'leaderBoard') => {
+  try {
+    const response = await axios.post(
+      'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games',
+      { name: name }
+    );
+    return response;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const message = (message = 'Data Inserted successfully.') => {
+  console.log(message);
+};
+
+const store = async (studentName = '', studentScore = 0, gameID) => {
+  try {
+    const response = await axios.post(
+      `https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/${gameID}/scores/`,
+      { user: studentName, score: studentScore }
+    );
+  } catch (error) {
+    console.error(error);
+  }
 };
 
 form.addEventListener('submit', (event) => {
   event.preventDefault();
   const name = nameInput.value;
   const score = scoreInput.value;
-  if (existGame()) {
+  if (gameID) {
     console.log('defined');
   } else {
-    setGame('LeaderBoard Game');
+    setGame('LeaderBoard Game').then(() => {
+      store(name, score, gameID);
+    });
   }
 });
