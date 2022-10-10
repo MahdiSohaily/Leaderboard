f13b873dc7c4681821186072c5cbb0163bf03c8f
MahdiSohaily <mahdi.sohaily4030@gmail.com>
2022-10-10 10:08:23 +0430

    Add axios library

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
+/* eslint-disable linebreak-style */
 import './CSS/styles.css';
+
+const form = document.querySelector('.form');
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  
+});
