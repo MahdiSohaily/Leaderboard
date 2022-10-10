54d27460e1501fa27a1c056d5665c3d8a8b70b0c
MahdiSohaily <mahdi.sohaily4030@gmail.com>
2022-10-10 10:21:02 +0430

    Add check game function

--- src/index.js
+++ src/index.js
@@ -2,8 +2,21 @@
 import './CSS/styles.css';
 
 const form = document.querySelector('.form');
+const nameInput = document.querySelector('.name');
+const scoreInput = document.querySelector('.score');
+
+const existGame = () => (localStorage.getItem('game') ? JSON.parse(localStorage.getItem('game')) : false);
+const setGame = () => {
+    
+};
 
 form.addEventListener('submit', (event) => {
   event.preventDefault();
-  
+  const name = nameInput.value;
+  const score = scoreInput.value;
+  if (existGame()) {
+    console.log('defined');
+  } else {
+    setGame('LeaderBoard Game');
+  }
 });
