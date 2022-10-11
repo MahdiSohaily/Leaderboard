6b06bbd5f22a2110e32cd98c6d05c5eb0959ca9a
MahdiSohaily <mahdi.sohaily4030@gmail.com>
2022-10-11 09:22:43 +0430

    Update JS & style file structure for best practice

--- src/modules/getData.js
+++ src/modules/getData.js
@@ -6,7 +6,7 @@ const getData = async (gameID) => {
   let response;
   try {
     response = await axios.get(
-      `https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/${gameID}/scores/`
+      `https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/${gameID}/scores/`,
     );
     return response;
   } catch (error) {
